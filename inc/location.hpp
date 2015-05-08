01360c3bc92dac9d25ec4df57f5d0f3ca4aa6b2c
Tom Barrett <tombarrett@cornell.engr.siu.edu>
2015-05-08 14:32:05 -0500

    changed the color of the basic resource so no confusion with dead bodies and fixed spacing

--- inc/location.hpp
+++ inc/location.hpp
@@ -5,11 +5,11 @@ class Location
 {
   public:
     Location(){x=y=type=0;}; //is this line needed?
-	  Location(int x1, int y1, int t1){x=x1;y=y1;type=t1;};
+	Location(int x1, int y1, int t1){x=x1;y=y1;type=t1;};
 
     int x; //x-coordinate of entity
-	  int y; //y-coordinate of entity
-	  int type; //value associated with type of entity at location. 1: Creature, 2: Resource
+	int y; //y-coordinate of entity
+	int type; //value associated with type of entity at location. 1: Creature, 2: Resource
 };
 
 #endif
