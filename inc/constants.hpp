ccb66c8d6d175b1bd3d8ecaa540e80baac5181b6
tom <tombarrett@siu.edu>
2017-01-19 13:42:36 -0600

    more entertaining constants

--- inc/constants.hpp
+++ inc/constants.hpp
@@ -1,9 +1,9 @@
 #ifndef constants_h
 #define constants_h
 
-const int CREATURES               = 10;
-const int RESOURCES               = 100;
-const int MINIMUM_RESOURCES       = 90;
+const int CREATURES               = 100;
+const int RESOURCES               = 1000;
+const int MINIMUM_RESOURCES       = 700;
 const int WINDOW_X                = 1080;
 const int WINDOW_Y                = 640;
 
@@ -15,13 +15,13 @@ const int CREATURE_MAX_HEALTH     = 1000;
 const int CREATURE_BEST_SENSE     = 100;
 const int CREATURE_SPEED          = 1;
 const int CREATURE_REACH          = 5;
-const int CREATURE_SIZE_MAX       = 20;
-const int CREATURE_SIZE_START     = 10;
+const int CREATURE_SIZE_MAX       = 5;
+const int CREATURE_SIZE_START     = 1;
 const int CREATURE_BITE           = 10;
 const int CREATURE_AMOUNT_TO_GROW = 50;
 
 const int RESOURCE_SIZE_START     = 5;
-const int RESOURCE_SIZE_MAX       = 10;
+const int RESOURCE_SIZE_MAX       = 2;
 const int RESOURCE_AMOUNT_START   = 100;
 const int RESOURCE_AMOUNT_MAX     = 200;
 const int RESOURCE_GROW           = 1;
