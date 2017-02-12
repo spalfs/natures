358ea01c03d787377ef88945706f350befd5b7a9
tom <tombarrett@siu.edu>
2017-02-12 06:39:09 -0600

    -added pause function

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main()
 
     Timer fps;
     int speed = 60;
+    bool pause = false;
 
 	while(e.gRun())
 	{
@@ -23,16 +24,19 @@ int main()
                     case SDLK_q:        e.off();                    break;
                     case SDLK_EQUALS:   speed+=30;                  break;
                     case SDLK_MINUS:    if(speed >30) speed-=30;    break;
+                    case SDLK_SPACE:    pause=(pause)?(0):(1);      break;
                 }
 		}
 
-		main.Clear();
-        
-        L.Remove();
-        L.Behavior();
-		L.Place();
+        if(!pause){
+            main.Clear();
+            
+            L.Remove();
+            L.Behavior();
+            L.Place();
 
-		main.Render();
+            main.Render();
+        }
 		
         if(fps.getTicks() < (1000 / speed))
             SDL_Delay((1000 / speed) - fps.getTicks());
